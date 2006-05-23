ebc1c7c6678a2f1323a88410ef6538afd7ac9ce2
intrigeri <intrigeri@localhost>
2006-05-23 17:23:55 +0000

    lunar-mode-line : +keywords

--- lunar-mode-line.el
+++ lunar-mode-line.el
@@ -9,7 +9,8 @@
 ;; Status: Works in Emacs 22
 ;; Created: 2006-05-22
 
-;; $Id: lunar-mode-line.el 2 2006-05-23 17:22:58Z intrigeri $
+;; $Id: lunar-mode-line.el 3 2006-05-23 17:23:55Z intrigeri $
+;; $URL: https://intrigeri.boum.org/svn/pub/lunar-mode-line/lunar-mode-line.el $
 
 ;; This program is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the
